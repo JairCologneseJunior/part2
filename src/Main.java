a2d9440183ba908d75aac181621d4fac5ed2443b
unknown <Administrador@JVN0194015750.jv01.local>
2022-11-16 14:58:21 -0300

    parte dois dos exercicios

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//        System.out.printf("Digite sua  nota");
+//        Scanner nota = new Scanner(System.in);
+//        int nota1 = nota.nextInt();
+//
+//
+//        System.out.printf("Digite sua nota");
+//        Scanner notaa = new Scanner(System.in);
+//        int nota2 = nota.nextInt();
+//
+//        System.out.printf("Digite sua nota");
+//        Scanner notaaa = new Scanner(System.in);
+//        int nota3 = nota.nextInt();
+//
+//        System.out.printf("Digite sua nota");
+//        Scanner notaaaa = new Scanner(System.in);
+//        int nota4 = nota.nextInt();
+//
+//
+//        int total = (nota1 + nota2 + nota3 + nota4)/4 ;
+//
+//        if (total > +7) {
+//            System.out.printf("parabens voce tem nots suficiente %d ", total);
+//        } else if (total < 7) {
+//            System.out.printf("voce nao tem nota suficiente %d", total);
+//        }
+        //at2
+
+
+    }
+}
