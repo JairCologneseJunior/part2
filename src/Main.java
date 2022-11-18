7ecaed9c3e0f382ef21ddd5e3b7769819bd460e1
JairCologneseJunior <jaircolognese2.0@gmail.com>
2022-11-18 12:59:23 -0300

    atualização

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 //        System.out.printf("Digite sua  nota");
@@ -21,13 +22,74 @@ public class Main {
 //
 //        int total = (nota1 + nota2 + nota3 + nota4)/4 ;
 //
-//        if (total > +7) {
+//        if (total >=7) {
 //            System.out.printf("parabens voce tem nots suficiente %d ", total);
-//        } else if (total < 7) {
+//        } else {
 //            System.out.printf("voce nao tem nota suficiente %d", total);
-//        }
+//      }
         //at2
+//teste
+//        System.out.printf("Digite a temperatura ");
+//        Scanner ler= new Scanner(System.in);
+//        String temp = ler.nextLine();
+//        System.out.printf("digite se a temperatura esta em Celsius (C)" +
+//                " ou se esta em fahrenheit (F) ");
+// parc >=3 || parc <=24
+//at3
 
 
-    }
+//        System.out.printf("Digite sua idade");
+//        Scanner idade = new Scanner(System.in);
+//        int idd = idade.nextInt();
+
+
+//        int trint = (sal * 30) /100;
+//       if (idd >=18){
+//           System.out.printf("Digite seu salario");
+//           Scanner salario = new Scanner(System.in);
+//           int sal = salario.nextInt();
+//
+//           System.out.printf("Digite em quantas parcelas voce quer pagar ");
+//           Scanner parcela = new Scanner(System.in);
+//           int parc = parcela.nextInt();
+//
+//
+//
+//           if ( parc >=3 && parc<=24 ){
+//               System.out.println("Digite o valor do imprestimo");
+//               Scanner imprestimo = new Scanner(System.in);
+//               int impr = imprestimo.nextInt();
+//
+//               int trint = (sal *30) /100;
+//               int nparc = impr /  parc;
+//
+//               if (nparc < trint){
+//                   System.out.printf("Voce tera que pagar %d por mes", nparc);
+//               }else if (nparc > trint){
+//                   System.out.printf("o valor exede 30% do seu salario");
+//
+//               }
+//        }
+//           else if (parc <3 && parc>24){
+//               System.out.printf("O minimo de parcelas sao 3 e max 24");
+//           }
+//
+//           else if ( idd <18){
+//           System.out.printf("Voce noa pode fazer o imprestimo por que vc e de menor ");
+//       }
+//    }
+        //at4
+//        Random numr = new Random();
+//        int numran = numr.nextInt(10);
+//        System.out.printf("Digite um numero de 0 a 10");
+//        Scanner ler =  new Scanner(System.in);
+//        int chute = ler.nextInt();
+//        if (chute != numran){
+//            System.out.printf("Voce errou");
+//
+//        }else{
+//            System.out.printf("vc acertou");
+//    }
 }
+}
+
